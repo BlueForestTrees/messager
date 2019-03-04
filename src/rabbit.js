d220bbda4f678185e3e43179ee19735d9666ef57
slim <smedini@gmail.com>
2019-03-04 12:01:42 +0100

    send debug

--- src/rabbit.js
+++ src/rabbit.js
@@ -63,7 +63,7 @@ var receiver = function (work, routingKey, qConf) {
                 try {
                     json = bson.deserialize(msg.content)
                 } catch (e) {
-                    console.error(e.message, msg.content)
+                    console.error(e.message, Buffer.from(msg.content))
                     return
                 }
                 try {
