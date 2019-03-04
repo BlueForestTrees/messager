4e415caae4d364b818b04a7e52ddb7d90536a29a
slim <smedini@gmail.com>
2019-03-04 15:11:19 +0100

    ack when deserialize exception.

--- src/rabbit.js
+++ src/rabbit.js
@@ -64,6 +64,7 @@ var receiver = function (work, routingKey, qConf) {
                     json = bson.deserialize(msg.content)
                 } catch (e) {
                     console.error(e.message, Buffer.from(msg.content).toString())
+                    ctx.ch.ack(msg)
                     return
                 }
                 try {
